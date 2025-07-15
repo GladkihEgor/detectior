4d80337bbd4980f606a6090d159b291a5e2649f7
Egor Gladkih <i@gladkihegor.ru>
2025-07-15 22:09:38 +0500

    add video capturing

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
 
 using namespace std;
+using namespace cv;
 
 int main()
 {
-  cout << "Hello subscribers" << endl;
+  Mat frame;
+  // auto source = "rtsp://localhost:8554/bunny";
+  auto source = 1;
+  auto cap = VideoCapture(source);
+  if (!cap.isOpened()) {
+    cerr << "ERROR: can't open video capture for " << source << endl;
+    return 1;
+  }
+
+  while (cap.read(frame)) {
+    imshow("Live", frame);
+    if (waitKey(5) >= 0)
+        break;
+  }
+
   return 0;
 }
--- nob.c
+++ nob.c
@@ -5,7 +5,9 @@ int main(int argc, char **argv)
 {
   NOB_GO_REBUILD_URSELF(argc, argv);
   Nob_Cmd cmd = {0};
-  nob_cmd_append(&cmd, "clang++", "-Wall", "-Wextra", "-o", "main", "main.cpp");
+  const char *include = "-I/opt/homebrew/Cellar/opencv/4.11.0_1/include/opencv4/";
+  const char *libs = "-L/opt/homebrew/Cellar/opencv/4.11.0_1/lib/";
+  nob_cmd_append(&cmd, "clang++", "-Wall", "-Wextra", include, libs, "-lopencv_core", "-lopencv_videoio", "-lopencv_highgui", "-o", "main", "main.cpp");
   if (!nob_cmd_run_sync(cmd)) return 1;
   return 0;
 }
