7973556d2bf15300f54a1d0585e0064bf1da7ac4
Egor Gladkih <i@gladkihegor.ru>
2025-07-17 12:14:37 +0500

    refactor main compile cmd

--- nob.c
+++ nob.c
@@ -6,10 +6,14 @@ int main(int argc, char **argv)
 {
   NOB_GO_REBUILD_URSELF(argc, argv);
   Cmd cmd = {0};
-  const char *include = "-I/opt/homebrew/Cellar/opencv/4.11.0_1/include/opencv4/";
-  const char *libs = "-L/opt/homebrew/Cellar/opencv/4.11.0_1/lib/";
-  cmd_append(&cmd, "clang++", "-Wall", "-Wextra", include, libs, "-lopencv_core", "-lopencv_videoio", "-lopencv_highgui", "-o", "main", "main.cpp");
-  if (!cmd_run_sync(cmd)) return 1;
+  cmd_append(&cmd, "clang++");
+  cmd_append(&cmd, "-Wall", "-Wextra");
+  cmd_append(&cmd, "-I/opt/homebrew/Cellar/opencv/4.11.0_1/include/opencv4/");
+  cmd_append(&cmd, "-o", "main");
+  cmd_append(&cmd, "main.cpp");
+  cmd_append(&cmd, "-L/opt/homebrew/Cellar/opencv/4.11.0_1/lib/");
+  cmd_append(&cmd, "-lopencv_core", "-lopencv_videoio", "-lopencv_highgui");
+  if (!cmd_run_sync_and_reset(&cmd)) return 1;
   return 0;
 }
 
