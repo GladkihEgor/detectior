7c7b5ee17eb3123bb7b98f6f2f352b02ad07117d
GladkihEgor <i@gladkihegor.ru>
2025-07-27 23:38:04 +0500

    update opencv paths

--- nob.c
+++ nob.c
@@ -9,10 +9,10 @@ int main(int argc, char **argv)
   Cmd cmd = {0};
   cmd_append(&cmd, "clang++");
   cmd_append(&cmd, "-Wall", "-Wextra");
-  cmd_append(&cmd, "-I/opt/homebrew/Cellar/opencv/4.11.0_1/include/opencv4/");
+  cmd_append(&cmd, "-I/opt/homebrew/Cellar/opencv/4.12.0/include/opencv4/");
   cmd_append(&cmd, "-o", "main");
   cmd_append(&cmd, "main.cpp");
-  cmd_append(&cmd, "-L/opt/homebrew/Cellar/opencv/4.11.0_1/lib/");
+  cmd_append(&cmd, "-L/opt/homebrew/Cellar/opencv/4.12.0/lib/");
   cmd_append(&cmd, "-lopencv_core", "-lopencv_videoio", "-lopencv_highgui", "-lopencv_imgproc", "-lopencv_dnn");
   if (!cmd_run_sync_and_reset(&cmd)) return 1;
   return 0;
